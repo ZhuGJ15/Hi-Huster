9e20bb9535b764b63c8dc1a7b70fdec62a9eb39e
Asherz <helloasher@gmail.com>
2022-07-30 01:57:11 +0800

    Create Project

---
+++ hi-controller/src/main/java/org/asherz/hi/controller/api/BarApi.java
@@ -0,0 +1,11 @@
+package org.asherz.hi.controller.api;
+
+/**
+ * Copyright zhuganjun ©. All Rights Reserved.
+ *
+ * @author: zhuganjun
+ * @date: 2022/7/30 1:49
+ * @description:
+ */
+public interface BarApi {
+}
---
+++ hi-controller/src/main/java/org/asherz/hi/controller/api/PermissionApi.java
@@ -0,0 +1,11 @@
+package org.asherz.hi.controller.api;
+
+/**
+ * Copyright zhuganjun ©. All Rights Reserved.
+ *
+ * @author: zhuganjun
+ * @date: 2022/7/30 1:48
+ * @description:
+ */
+public interface PermissionApi {
+}
